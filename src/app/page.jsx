913a5291d602696a5abbeb4292134b0f8179f2b4
Pierre Marquet <marquet_pierre@yahoo.fr>
2023-05-15 14:32:50 +0200

    modify page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,13 +2,13 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-6xl font-bold text-center mb-14">Gestion de vos stock</h1>
-      <table className="w-1/2 mx-auto mb-7">
+      <table className="w-5/6 mx-auto mb-7">
         <thead>
           <tr>
-            <th className="border border-black w-2/6">Nom</th>
+            <th className="border border-black w-3/6">Nom</th>
             <th className="border border-black w-1/6">Quantité</th>
             <th className="border border-black w-1/6">Prix</th>
-            <th className="border border-black w-2/6">Action</th>
+            <th className="border border-black">Action</th>
           </tr>
         </thead>
         <tbody>
@@ -17,8 +17,10 @@ export default function Home() {
             <td className="border border-black"></td>
             <td className="border border-black"></td>
             <td className="border border-black">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Modifier</button>
-              <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Supprimer</button>
+              <span className="w-max flex">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-1">Modifier</button>
+                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Supprimer</button>
+              </span>
             </td>
           </tr>
         </tbody>
