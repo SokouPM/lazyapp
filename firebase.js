a2c34f1df668835e3d3a9b31b0be87796fab6cb5
Pierre Marquet <marquet_pierre@yahoo.fr>
2023-05-15 14:04:35 +0200

    add firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyANL6He6Li2Kxd3lu5hNM0WfkvH7yifkak",
+  authDomain: "lazyapp-8f0c9.firebaseapp.com",
+  projectId: "lazyapp-8f0c9",
+  storageBucket: "lazyapp-8f0c9.appspot.com",
+  messagingSenderId: "275294672543",
+  appId: "1:275294672543:web:51c55ac9292026db7f34aa",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
